0d965f4c405accfb057411603c3d03329da177ef
MoonquakesX <3180104947@zju.edu.cn>
2023-05-15 10:57:49 +0800

    Normalize URL format

--- V0/server.go
+++ V0/server.go
@@ -9,12 +9,14 @@ func Register(BasicRouter *gin.Engine) {
 	BasicRouter.GET("login", API.Login)
 	apiGroup := BasicRouter.Group("api")
 	jobsGroup := apiGroup.Group("jobs")
-	jobsGroup.POST("/polish", API.BatchPolishJobs)
-	jobsGroup.POST("/collect", API.CollectJob)
-	jobsGroup.POST("/pull-off", API.BatchPullOffJobs)
-	jobsGroup.DELETE("/de-collect", API.DeCollectJob)
-	jobsGroup.GET("/ListViewedJobs", API.ListViewedJobs)
-	jobsGroup.GET("/ListUploadedJobs", API.ListUploadedJobs)
-	jobsGroup.GET("/ListCollectedJobs", API.ListCollectedJobs)
-
+	jobsGroup.Use(AuthJWT())
+	{
+		jobsGroup.POST("/polish", API.BatchPolishJobs)
+		jobsGroup.POST("/collect", API.CollectJob)
+		jobsGroup.POST("/pull-off", API.BatchPullOffJobs)
+		jobsGroup.DELETE("/de-collect", API.DeCollectJob)
+		jobsGroup.GET("/list-views", API.ListViewedJobs)
+		jobsGroup.GET("/list-uploads", API.ListUploadedJobs)
+		jobsGroup.GET("/list-collections", API.ListCollectedJobs)
+	}
 }
